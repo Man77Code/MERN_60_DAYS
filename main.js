11e8f3f2b944570e2ea734001a87c1879b0f8ff4
Man77Code <143287393+Man77Code@users.noreply.github.com>
2026-04-04 20:10:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Module and packages
+// console.log("ER Aarav");
+// console.log("He Fullstack Developer")
+
+// const x = require("./date")
+// console.log(x); // 6
+
+// const data = require("./date");
+// console.log(data); // { x: 6, y: 7, z: 10 }
+
+// const getDate = require("./date");
+// getDate() // 2026-04-04T14:30:45.909Z
+
+const date = require("./date")
+console.log(date);
+date.getDate(); // 2026-04-04T14:36:14.091Z
+date.getTime(); // 8:06:26 pm
